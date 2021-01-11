c1424eea973e33bb97db7e18fa19302c6008c5fc
Jeferson Almeida <jefeson.almeida@hotmail.com>
2021-01-11 01:22:16 -0300

    Finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
 const API_URL = "http://localhost:8080";
 const mapboxToken = "pk.eyJ1IjoiamVmZmRydW1tIiwiYSI6ImNranJ4MGV1YTAwNXkzMHF4aXh2Zjg3ejYifQ.EB43i9rvoaESRB6tYe0ICw";
@@ -8,4 +9,8 @@ export function fetchProdutcts(){
 
 export function fetchLocalMapBox(local: string){
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saverOrder(payload: OrderPayload) {
+    return axios.post(`${API_URL}/orders`, payload);
 }
